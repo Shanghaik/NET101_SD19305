6468655f0d260c7e92ed18ca72239d5034e1cc10
shanghaik168@gmail.com <Shanghaik1998>
2024-01-27 09:13:46 +0700

    oop4TC

---
+++ 8_4TC_OOP/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _8_4TC_OOP
+{
+    /*
+     * OOP có 4 tính chất cơ bản
+     * 1. Đóng gói: Thể hiện khả năng truy cập đến các 
+     * thành phần trong Class. VD: Khi đóng cửa lớp (private) thì
+     * sinh viên bên ngoài không thể vào được, cổng trường luôn
+     * mở (public) thì ai cũng có thể vào được
+     * 2. Kế thừa: Thể hiện khả năng sử dụng lại các tành phần
+     * của các lớp đã được xây dựng. VD: Kế thừa lại khả năng chém
+     * gió của thầy Dũng =))
+     * 3. Đa hình: Các thể hiện khác nhau của cùng 1 hành động (hành vi)
+     * trong các trường hợp khác nhau giữa các đối tượng khác nhau hoặc
+     * cùng 1 đối tượng nhưng trong các điều kiện khác nhau.
+     * VD: cùng là chạy bộ tập thể dục nhưng bạn A thì chạy nhanh, B thì
+     * chạy rất nhanh, C thì chạy chậm
+     * Bạn A, khi chạy ra chợ mua rau sẽ khác bạn A khi bị bố đuổi về từ
+     * quán net
+     * 4. Trừu tượng: Khả năng trừu tượng hóa các thành phần của 1 lớp
+     * để làm khuôn mẫu cho các lớp khác. VD: Lớp Cá có các hành động Bơi
+     * Ăn, các hành động này trừu tượng và không thể xác định rõ khi chưa
+     * biết chính xác đó là cá gì?
+     * 
+     */
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
